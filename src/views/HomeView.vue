a1c4a6eb09c72a72aa7da3267ecc6206f577b5f6
Carlos Ferrús Ferri <cmff1@alu.ua.es>
2022-06-02 13:44:27 +0200

    Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <b-jumbotron header="ISS" lead="Welcome to the secure password manager that also manages files!">
+      <p>To access each of its functionality, press the buttons in the header</p>
+    </b-jumbotron>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'HomeView',
 }
